26e7b9d538df4709826ecddc5a0fb05265c152df
AvaBlix® <ruinbad00@gmail.com>
2025-11-02 11:47:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+"""
+InstaOsint Configuration File
+
+INSTRUCTIONS:
+1. Fill in your Instagram credentials below
+2. Save the file
+3. The tool will automatically use these credentials
+
+WARNING: Use a dummy account only!
+This tool may violate Instagram's Terms of Service.
+"""
+
+INSTAGRAM_CREDENTIALS = {
+    'username': '',  # Add your username here
+    'password': ''   # Add your password here
+}
+
+"""
+Example (remove this comment when adding real credentials):
+INSTAGRAM_CREDENTIALS = {
+    'username': 'my_dummy_account',
+    'password': 'my_dummy_password123'
+}
+"""
